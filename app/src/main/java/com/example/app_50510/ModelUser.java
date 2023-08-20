336710331acd29840fbd4a924ff74bd825bfcd37
javi <ingenierodesoftware73@gmail.com>
2023-08-19 21:44:44 -0500

    fix error to run app

---
+++ app/src/main/java/com/example/app_50510/ModelUser.java
@@ -0,0 +1,31 @@
+package com.example.app_50510;
+
+public class ModelUser {
+    private  String name;
+    private int id;
+    private String birthDay;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+}
